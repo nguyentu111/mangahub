eb90ba456130ccb417593560f8715ff74be11b0d
nguyentu111 <tunguyen118108@gmail.com>
2023-01-11 11:31:35 +0700

    test

--- frontend/src/pages/comic/read/[...slug].tsx
+++ frontend/src/pages/comic/read/[...slug].tsx
@@ -100,7 +100,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     const {
       slug: [mangaSlug, chapterSlug],
     } = context.params as Params;
-    const { data } = await axiosClient.get(
+    const data = await axiosClient.get(
       "lhmanga/comic/" + mangaSlug + "/" + chapterSlug
     );
     if (!data)
@@ -109,7 +109,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
       };
     else {
       return {
-        props: { data },
+        props: { data: data.data },
         revalidate: 18000,
       };
     }
